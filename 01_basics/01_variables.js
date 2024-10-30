7b0a17b7932eb16bbe6a8ab4a197e9e3da55e129
Dipangshu-Kundu <146168899+Dipangshu-Kundu@users.noreply.github.com>
2024-10-30 06:55:33 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="abc@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;//by default undefined
+// const cannot be changed
+//for variable declaration we use let and var
+//and we should we use let and not var because of the scope resolution problem
+// accountId=2
+accountEmail="hc@hc.com"
+accountPassword="1313132"
+accountCity="bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
