d29bcd7f9dd749dac1ef45ab2da249cb1c5b812e
Dipangshu-Kundu <146168899+Dipangshu-Kundu@users.noreply.github.com>
2025-02-21 16:03:31 +0000

    dataconversion

---
+++ 01_basics/l6_datatypes.js
@@ -0,0 +1,25 @@
+let score="33a"
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber=Number(score)
+console.log(valueInNumber)
+console.log(typeof(valueInNumber))
+let score1=null
+console.log(Number(score1))
+
+//"33"=>33
+//"33a"=>NaN(not a number but its type is number)
+//true=>1 false=>0
+
+let isLogIn=1
+let new1=""
+let new2="dipangshu"
+let booleanisLogIn=Boolean(isLogIn)
+console.log(booleanisLogIn)
+console.log(Boolean(new1))
+console.log(Boolean(new2))
+
+let some=33
+let stringnum=String(some)
+console.log(stringnum)
+console.log(typeof(stringnum))
