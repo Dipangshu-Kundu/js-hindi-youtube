d0326282687934ae651fb1be0e273ab657683e56
Dipangshu-Kundu <146168899+Dipangshu-Kundu@users.noreply.github.com>
2025-02-22 16:24:32 +0000

    strings

---
+++ 01_basics/l11_strings.js
@@ -0,0 +1,31 @@
+ console.log("hello"+"world");
+ const name1="dipangshu"
+ const count=50
+ console.log(name1+count);
+ console.log(`the name is ${name1} and my repo count is ${count}`);
+const game=new String("bgmi");
+console.log(game);
+console.log(game.__proto__);
+console.log(game.length);
+console.log(game.toUpperCase());
+console.log(game.charAt(2));
+console.log(game.indexOf('m'));
+let newStr=game.split('g');
+console.log(newStr);
+
+let new1Str=game.substring(0,2);
+console.log(new1Str);
+
+let new2str=game.slice(-3,-2);
+console.log(new2str);
+
+let game1=" bgmi free fire "
+console.log(game1.trim());
+
+console.log(game1.trim().replaceAll('e','d'));
+console.log(game1.includes('free'));
+
+
+
+ 
+ 
