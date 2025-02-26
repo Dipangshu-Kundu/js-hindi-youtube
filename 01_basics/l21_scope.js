91afdf1b04059a32b5e192c53da052a70cb993a0
Dipangshu-Kundu <146168899+Dipangshu-Kundu@users.noreply.github.com>
2025-02-26 18:03:57 +0000

    scope

---
+++ 01_basics/l21_scope.js
@@ -0,0 +1,15 @@
+let a=300
+if(true)
+{
+    let a=10
+    const b=20
+    console.log(a);
+    
+}
+// console.log(c);
+console.log(a);
+// console.log(b);
+for(let i=0;i<10;i++)
+{
+    //
+}
---
+++ 01_basics/l22_scope_hoisting.js
@@ -0,0 +1,36 @@
+function one(){
+    const username="hitesh"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+if(true)
+{
+    const username="hitesh"
+    if(username==="hitesh")
+    {
+        const website="youtube"
+        console.log(username+website);
+    }
+    console.log(website);//not accessible
+    
+}
+console.log(username);//not accessible
+
+
+//++++++++++++++++++++++++interesting+++++++++++++++++++++
+
+function addone(num){
+    return num+1;
+}
+addone(5)
+
+const addtwo=function(num){
+    return num+2;
+}
+addtwo(5)
