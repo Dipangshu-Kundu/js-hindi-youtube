3b04e777b6a697f768b35c511d9e1ca517efbeaa
Dipangshu-Kundu <146168899+Dipangshu-Kundu@users.noreply.github.com>
2025-03-04 12:36:14 +0000

    async

---
+++ 01_basics/l38_projects_with_Async.js
@@ -0,0 +1,27 @@
+//https://stackblitz.com/edit/dom-project-chaiaurcode-gh1nvcgh?file=4-GuessTheNumber%2Findex.html,4-GuessTheNumber%2Fchaiaurcode.js
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <h1>chai aur code</h1>
+//     <button id="start">Start</button>
+//     <button id="stop">Stop</button>
+// </body>
+// <script>
+//     let intervalid;
+//     document.querySelector('#start').addEventListener('click',()=>{
+//        intervalid=setInterval((str)=>{
+//         console.log(str,Date.now());
+//     },1000,"hi")    
+//     })
+    
+//     document.querySelector('#stop').addEventListener('click',()=>{
+//         clearInterval(intervalid)
+//         })
+// </script>
+// </html>
